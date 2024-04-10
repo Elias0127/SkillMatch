15f806c2385f5d3fab8404826631f0cf757908df
Elias0127 <eliasmekonen001@gmail.com>
2024-04-10 07:08:07 -0700

    Delete unused files and added django default files

---
+++ skillmatch_django/skillmatch_app/app.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SkillmatchAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'skillmatch_app'
---
+++ skillmatch_django/skillmatch_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Define your URL patterns here
+]
