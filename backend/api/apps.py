d30bfaa0f52f410ffd6e95b545027adda768eaa4
Elias0127 <eliasmekonen001@gmail.com>
2024-04-10 17:23:36 -0700

    Restructured project with new backend/frontend and added 'api' app

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SkillmatchAppConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'skillmatch_app'
+    name = 'api'
