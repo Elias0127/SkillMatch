25b495ce6815835532cefc204cae3df1f31236f8
Hamzah Qutob <hqutob.19@gmail.com>
2024-04-14 23:17:22 -0700

    created a function to fetch the profile data

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,3 +1,21 @@
-export default function Profile(){
-    return <h1>Profile</h1>
+import * as React from "react";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function EProfile(){
+    const [profile, setProfile] = useState({
+        firstName:'',
+        lastName: '',
+        phoneNum: '',
+        email: '',
+        location: ''
+    });
+
+    const navigate = useNavigate();
+
 }
+
+
+
