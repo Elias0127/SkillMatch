ed456baed97b90dd3af88b0d3bef5695a864fe14
Hamzah Qutob <hqutob.19@gmail.com>
2024-04-14 23:32:22 -0700

    added basic front end for the profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -14,7 +14,60 @@ function EProfile(){
     });
     const navigate = useNavigate();
 
+    return (
+        <div className="employer-profile-container">
+                <form onSubmit={}>
+                    <div>
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            value={profile.firstName}
+                            onChange={}
+                        />
+                    </div>
+                    <div>
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            name="lastName"
+                            value={profile.lastName}
+                            onChange={}
+                        />
+                    </div>
+                    <div>
+                        <label>Phone Number</label>
+                        <input
+                            type="tel"
+                            name="phoneNum"
+                            value={profile.phoneNum}
+                            onChange={}
+                        />
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={profile.email}
+                            onChange={}
+                        />
+                    </div>
+                    <div>
+                        <label>Location</label>
+                        <input
+                            type="text"
+                            name="location"
+                            value={profile.location}
+                            onChange={}
+                        />
+                    </div>
+                    <button type="submit">Update Profile</button>
+                </form>
+            </div>
+        );
 }
 
+export default EProfile;
 
 
