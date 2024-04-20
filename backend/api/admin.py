693e7d8101c30fc4e02ccf800163964c259a7eb0
Elias0127 <eliasmekonen001@gmail.com>
2024-04-19 20:53:10 -0700

    Add custom admin for Profile model and static media URLs in API URLs

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.utils.html import mark_safe  # Allows safe HTML rendering
 from .models import User, Profile, WorkerProfile, Skill, WorkerSkill, EmployerProfile, Contract
 
+# Custom admin for the Profile model to show pictures
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'picture_preview')
+    readonly_fields = ('picture_preview',)
+
+    def picture_preview(self, obj):
+        if obj.picture:
+            return mark_safe(f'<img src="{obj.picture.url}" width="150" />')
+        return "No Image Uploaded"
+    picture_preview.short_description = "Picture Preview"
+
+
+# Register models
 admin.site.register(User)
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(WorkerProfile)
 admin.site.register(Skill)
 admin.site.register(WorkerSkill)
