c4d86e9484799c9b615e719e5f1adb46f01320b8
Hamzah Qutob <hqutob.19@gmail.com>
2024-04-20 18:13:00 -0700

    created a prototype style for now and fixed up some things with the header and the hero-section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/Home.css";
+import mainImage from "../assets/Chatbot.png";
 import Logout from './Logout'; 
 
 const Header = () => {
         return (
-          <header className="header">
+            <div className="header">
             <nav className="nav-bar">
-              <Link to="/" className="nav-item">Skillmatch</Link>
-              <Link to="/about" className="nav-item">About</Link>
-              <Link to="/discover" className="nav-item">Discover</Link>
-              <Link to="/review" className="nav-item">Review</Link>
-              <Link to="/login" className="nav-item nav-button">Log In</Link>
-              <Link to="/signup" className="nav-item nav-button sign-up">Sign Up</Link>
+                <div className="left-section">
+                    <Link to="/Home" className="nav-item">Skillmatch</Link>
+                    <Link to="/about" className="nav-item">About</Link>
+                    <Link to="/discover" className="nav-item">Discover</Link>
+                    <Link to="/review" className="nav-item">Review</Link>
+                </div>
+                <div className="right-section">
+                    <div className="auth-buttons">
+                        <Link to="/login" className="nav-button">Log In</Link>
+                        <Link to="/register" className="nav-button sign-up">Sign Up</Link>
+                    </div>
+                </div>
             </nav>
-          </header>
+        </div>
+        
         );
 };
 
 const HeroSection = () => {
 return (
     <section className="hero-section">
-    <h1>Your local service needs</h1>
-    <Link to="/discover" className="discover-button">Discover</Link>
+        <div className='hero-content'>
+            <h1>Your local service needs</h1>
+            <Link to="/discover" className="discover-button">Discover</Link>
+        </div>
+        <div className='hero-image'>
+            <img src={mainImage} alt='main-image' />
+        </div>
     </section>
     );
 };
