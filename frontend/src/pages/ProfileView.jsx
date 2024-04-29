d044644345522710a00ceaaeb4e785764c04c23b
ttoanttran <ttoantran603@gmail.com>
2024-04-29 02:38:01 -0700

    Refactor Home button

--- frontend/src/pages/ProfileView.jsx
+++ frontend/src/pages/ProfileView.jsx
@@ -110,12 +110,12 @@ function ProfileView({ user }) {
 
     const BackIcon = () => {
         return (
-            <a href="/" className="home-link">
+            <>
                 <svg className="back-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" width="20" height="20">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                 </svg>
                 <span className="home-text">Home</span>
-            </a>
+            </>
         )
     }
 
