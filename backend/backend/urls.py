b3c9fdb511844af11e8ed0b9f3cc47bd1e1578bd
Elias0127 <eliasmekonen001@gmail.com>
2024-04-28 18:11:17 -0700

    Add static media configuration to backend/urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import UserRegistrationView, LoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,3 +11,4 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),  # JWT token endpoint
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),  # JWT token refresh endpoint
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
