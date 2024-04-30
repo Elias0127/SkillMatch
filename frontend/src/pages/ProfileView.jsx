54bbbd7ccc5deb8dd32e8889f62ad498aaac96b4
Toan Tran <toanphtran@gmail.com>
2024-04-29 22:55:58 -0700

    Fixed home and logout navs

--- frontend/src/pages/ProfileView.jsx
+++ frontend/src/pages/ProfileView.jsx
@@ -118,7 +118,7 @@ function ProfileView({ user }) {
                     </div>
                 </div>
                 <div className='menu'> 
-                    <a href="/home" className='menu-links'>
+                    <a href="#" className={activeSection === 'home' ? 'menu-links active' : 'menu-links'} onClick={() => handleMenuClick('home')}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" width="20" height="20" className="back-icon">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
                         Home
@@ -149,14 +149,23 @@ function ProfileView({ user }) {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" /></svg>
                         Notification
                     </a>
-                    <a href="#" className={activeSection === 'logout' ? 'menu-links active' : 'menu-links'} onClick={() => handleMenuClick('logout')}>
-                        <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" className="back-icon"><path d="M14 16l4-4m0 0l-4-4 m4 4h-11m11 5v1a2 2 0 01-2 2H6a3 3 0 01-3-3V7a3 3 0 013-3h10a2 2 0 0 12 2v1"></path></svg>
-                        Log out
-                    </a>
+                    <a href="#"><Logout></Logout></a>
+
                 </div>
             </div>
 
             <div className="main-container">
+                {activeSection === 'home' && (
+                    <div id="home-section" className="section">
+                         <div className='account-header'>
+                            <h2 className='account-title'>Hey, {formData.firstName}! </h2>
+                            <p className='account-text'>This is your profile dashboard with all your information as a <span className='role-title'>{role}.</span></p>
+                        </div>
+                    </div>                   
+                )}
+
+
+
                 {/* Account Section */}
                 {activeSection === 'account' && (
                 <>
