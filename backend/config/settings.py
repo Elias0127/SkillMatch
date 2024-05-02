85f9e80999f80bbc8c50917f8d3e198c3d32bfd4
Elias0127 <eliasmekonen001@gmail.com>
2024-05-02 09:47:27 -0700

    Add dj-database-url to requirements.txt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 load_dotenv()
