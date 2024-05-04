e866af498b571688d7e1d19e83882504cc46907c
Elias0127 <eliasmekonen001@gmail.com>
2024-05-03 21:59:59 -0700

    Update database configuration to use PostgreSQL in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,9 +126,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'skillmatch', 
+        'USER': 'myusername',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
