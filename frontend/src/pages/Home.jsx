d833ac5daedda87565b498ec51c7655e67b22215
Elias0127 <eliasmekonen001@gmail.com>
2024-05-06 11:35:34 -0700

    Refactor Home component to conditionally render NearbyWorkers based on role

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import NearbyWorkers from '../components/NearbyWorkers';
 
 const HomePage = () => {
+    const { role } = useParams();
+    console.log("Role:", role);
     return (
         <div>
             <h1>Welcome to the Homepage</h1>
-            <NearbyWorkers />
+        
+            {role === 'employer' && <NearbyWorkers />}
         </div>
     );
 };
